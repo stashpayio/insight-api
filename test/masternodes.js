edaf4bb5d0cbe75fc6de5d9fb6bddc8f1f525b75
BeachM <11566409+BeachM@users.noreply.github.com>
2018-09-26 17:17:22 -0700

    Stashify

--- test/masternodes.js
+++ test/masternodes.js
@@ -54,7 +54,7 @@ describe('Masternode', function() {
 		}];
 		var node = {
 			services: {
-        dashd: {
+        stashd: {
 					getMNList: sinon.stub().callsArgWith(0, null, MNList)
 				}
 			}
